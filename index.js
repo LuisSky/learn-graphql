f93cf4042321750c62634bdae0b67431e365b25f
luissky <luizguilhermeabrantes@gmail.com>
2023-09-11 22:35:34 -0300

    feat my first contact with qpl

---
+++ index.js
@@ -0,0 +1,26 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+  type Query {
+    ola: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    ola() {
+      return 'Hello World'
+    }
+  }
+}
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+
+server.listen(4000).then((url) => {
+  console.log('Servidor rodando em ${url}')
+})
