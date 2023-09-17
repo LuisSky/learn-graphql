fa535c4218a1fb1487cd9bda6d161d9e8fe8160a
luissky <luizguilhermeabrantes@gmail.com>
2023-09-16 23:48:15 -0300

    feat: added news types and start manipulation this

--- index.js
+++ index.js
@@ -3,15 +3,33 @@ const { ApolloServer, gql } = require('apollo-server')
 const typeDefs = gql`
   scalar Date
   
+  type User {
+    name: String
+    age: Int
+    mail: String
+  }
   type Query {
     datenow: Date
+    usuario: User
   }
 `
 
 const resolvers = {
+  User: {
+    mail(obj) {
+      return 'user@mail.com'
+    }
+  },  
   Query: {
     datenow() {
       return `${new Date()}`
+    },
+    usuario () {
+      return {
+        name: 'John Doe',
+        age: 26,
+        other: 'test'
+      }
     }
   }
 }
